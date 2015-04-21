b37c2e8db23832865f8f9910cd4f47c32c226c43
a13sergiocc <sergio.a.secas@hotmail.com>
2015-04-21 22:04:33 +0200

    Versionado de API
    Cración de grupo de rutas para el versionado de la API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,16 +22,29 @@ Route::controllers([
 ]);
 */
 
-// Ruta inicial
-Route::get('/', function() {
-	return 'Bienvenido API RESTful de aviones.';
+
+// Versiones de la API /api/v1.0/ruta
+
+Route::group(array('prefix' => 'api/v1.0'), function() {
+	// Ruta inicial
+	Route::get('/', function() {
+		return 'Bienvenido API RESTful de aviones.';
+	});
+	
+	// Creamos rutas nuevas para los controllers
+	Route::resource('fabricantes', 'FabricanteController', ['except' => ['create', 'edit']]);
+
+	
+	// Recurso anidado fabricantes aviones
+	Route::resource('fabricantes.aviones', 'FabricanteAvionController', ['except' => ['create', 'edit', 'show']]);
+
+	// Ruta aviones
+	Route::resource('aviones', 'AvionController', ['only' => ['index', 'show']]);	
 });
 
-// Creamos rutas nuevas para los controllers
-Route::resource('fabricantes', 'FabricanteController', ['except' => ['create', 'edit']]);
+// Ruta inicial
+	Route::get('/', function() {
+		return '<a href="/api/v1.0">Versión 1.0 de la API</a>';
+});
 
-// Recurso anidado fabricantes aviones
-Route::resource('fabricantes.aviones', 'FabricanteAvionController', ['except' => ['create', 'edit', 'show']]);
 
-// Ruta aviones
-Route::resource('aviones', 'AvionController', ['only' => ['index', 'show']]);
